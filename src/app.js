71bd4858730f2f3734738da4a0019721e5cb5c60
abhay1010-afk <abhaytomar1010@gmail.com>
2025-11-18 20:05:25 +0530

    hello

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 const express=require("express");
 const app=express();
+app.use("/",(req,res)=>{
+    res.send("hello node js");
+})
 app.use("/test",(req,res)=>{
     res.send("hello wlcm to routing .");
 });
